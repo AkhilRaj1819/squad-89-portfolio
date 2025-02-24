2bba65bf33aa28b7cf51c2ed073d53f721c7c97e
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-02-24 21:17:28 +0530

    s89

--- squad-89-portfolio/src/App.jsx
+++ squad-89-portfolio/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <h1>SQUAD 89</h1>
+    <header>
+      <h2 className='title'>SQUAD 89</h2>
+      <div className='list-header'>
+        <li className='lists'>
+          <ul>Home</ul>
+          <ul>Journey</ul>
+          <ul>Mentors</ul>
+          <ul>Students</ul>
+          <ul>About</ul>
+        </li>
+      </div>
+    </header>
+    <div className='main-page'>
+      <p className='main-title'>GGU x KALVIUM</p>
+    <h1 className='sub-title'>SQUAD 89</h1>
+    <h3 className='quote'>Transforming ideas into reality through code and creativity</h3>
+    <div className='main-page-button-div'>
+      <button className='main-page-button'>View our work</button>
+      <button className='learn-more-button'>Learn more</button>
+    </div>
+    </div>
+    
     </>
   )
 }
